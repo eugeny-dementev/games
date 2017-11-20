cd77230a22c3cb71bef5076e0d4da835acdce3d7
Eugeny Dementev <eugeny.dementev@ya.ru>
2017-11-20 23:26:52 +0800

    handle render event

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,22 +1,37 @@
 extern crate piston;
 extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
 
 use piston::window::WindowSettings;
-use glutin_window::GlutinWindow;
-
 use piston::event_loop::{Events, EventLoop, EventSettings};
+use piston::input::RenderEvent;
+
+use glutin_window::GlutinWindow;
+use opengl_graphics::{OpenGL, GlGraphics};
 
 fn main() {
+    let opengl = OpenGL::V3_2;
     let settings = WindowSettings::new("Sudoku", [512; 2])
+        .opengl(opengl)
         .exit_on_esc(true);
         
     let mut window: GlutinWindow = settings.build()
         .expect("Could not create window");
+
     let event_settings = EventSettings::new()
         .lazy(true);
     let mut events = Events::new(event_settings);
 
-    while let Some(e) = events.next(&mut window) {
+    let mut gl = GlGraphics::new(opengl);
 
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                use graphics::{clear};
+
+                clear([1.0; 4], g);
+            });
+        }
     }
 }
