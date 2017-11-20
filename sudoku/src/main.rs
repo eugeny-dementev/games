38cedab2f0657e343ccc2a814c5451baeac0ed6a
Eugeny Dementev <eugeny.dementev@ya.ru>
2017-11-20 23:01:47 +0800

    make window openable

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,3 +1,20 @@
+extern crate piston;
+extern crate glutin_window;
+
+use piston::window::WindowSettings;
+use glutin_window::GlutinWindow;
+
+use piston::event_loop::{Events, EventSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let settings = WindowSettings::new("Sudoku", [512; 2])
+        .exit_on_esc(true);
+        
+    let mut window: GlutinWindow = settings.build()
+        .expect("Could not create window");
+    let mut events = Events::new(EventSettings::new());
+
+    while let Some(e) = events.next(&mut window) {
+
+    }
 }
