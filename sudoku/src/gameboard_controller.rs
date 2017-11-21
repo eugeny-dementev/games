aa925b6d8a4283b9dcc4530a45d54a786760f6b4
Eugeny Dementev <eugeny.dementev@ya.ru>
2017-11-21 23:36:15 +0800

    handle cursor position

--- sudoku/src/gameboard_controller.rs
+++ sudoku/src/gameboard_controller.rs
@@ -4,12 +4,24 @@ use GameBoard;
 
 pub struct GameBoardController {
     pub gameboard: GameBoard,
+    pub selected_cell: Option<[usize; 2]>,
+    pub cursor_pos: [f64; 2],
 }
 
 impl GameBoardController {
     pub fn new(gameboard: GameBoard) -> GameBoardController {
-        GameBoardController { gameboard }
+        GameBoardController {
+            gameboard,
+            selected_cell: None,
+            cursor_pos: [0.0; 2],
+        }
     }
 
-    pub fn event<E: GenericEvent>(&mut self, e: &E) {}
+    pub fn event<E: GenericEvent>(&mut self, e: &E) {
+        if let Some(pos) = e.mouse_cursor_args() {
+            self.cursor_pos = pos;
+
+            println!("sursor pos: {:?}", self.cursor_pos);
+        }
+    }
 }
